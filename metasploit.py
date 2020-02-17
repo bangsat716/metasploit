1bad36abe5edeba07be08b741892003aef2fff64
bangsat716 <61156264+bangsat716@users.noreply.github.com>
2020-02-18 02:09:15 +0700

    Add files via upload

---
+++ metasploit.py
@@ -0,0 +1,110 @@
+#python
+import os
+print ("\033[034mMETASPLOIT")
+os.system('clear')
+print ("""
+
+
+                         $$\                         
+                         $$ |                        
+$$$$$$\$$$$\   $$$$$$\ $$$$$$\    $$$$$$\   $$$$$$$\ 
+$$  _$$  _$$\ $$  __$$\\_$$  _|   \____$$\ $$  _____|
+$$ / $$ / $$ |$$$$$$$$ | $$ |     $$$$$$$ |\$$$$$$\  
+$$ | $$ | $$ |$$   ____| $$ |$$\ $$  __$$ | \____$$\ 
+$$ | $$ | $$ |\$$$$$$$\  \$$$$  |\$$$$$$$ |$$$$$$$  |
+\__| \__| \__| \_______|  \____/  \_______|\_______/ 
+                                                     
+                                                     
+                                                     
+          $$\           $$\   $$\                    
+          $$ |          \__|  $$ |                   
+ $$$$$$\  $$ | $$$$$$\  $$\ $$$$$$\                  
+$$  __$$\ $$ |$$  __$$\ $$ |\_$$  _|                 
+$$ /  $$ |$$ |$$ /  $$ |$$ |  $$ |                   
+$$ |  $$ |$$ |$$ |  $$ |$$ |  $$ |$$\                
+$$$$$$$  |$$ |\$$$$$$  |$$ |  \$$$$  |               
+$$  ____/ \__| \______/ \__|   \____/                
+$$ |                                                 
+$$ |                                                 
+\__|                                                 
+
+
+""")
+
+import sys, time
+def jalan (kata):
+        for e in kata:
+            sys.stdout.write(e)
+            sys.stdout.flush()
+            time.sleep(0.2)
+jalan('\033[031m          >>>>>>>>>The X Queens<<<<<<<<<<<               '.center(44))
+jalan('         >>>>>>>>>>TeaMCyber<<<<<<<<<<<                '.center(44))
+nama = input('Siapa Nama Anda?')
+jalan('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'.center(44))
+print ('Selamat Datang' , nama)
+jalan('\033[034m          >>>>>>>>>> metasploit <<<<<<<<<<<<<              '.center(44))
+import os
+os.system('clear')
+import os
+import sys
+import time
+print ('\033[031mmetas')
+os.system("clear")
+
+print ("""
+
+	88
+	  88
+	    88
+	      88
+		88
+		  88
+		    88
+	     	      88
+			88
+		      88
+		    88
+		  88
+		88		 Author  : The X Queens
+	      88		 Youtube : TeaM Cyber
+  	    88
+	  88 			 8888888888888888888888
+	88
+""")
+
+print ("\033[033m===================================")
+print ("          SELAMAT DATANG!!!")
+print ("===================================")
+time.sleep(0.5)
+print ("\033[035m  [\033[037m01\033[035m] \033[034mInstall MetasploitV5\033[037m (\033[033mOS\033[032m Versi 7.0 8.0 9.0\033[037m) \033[037m")
+print ("\n")
+print ("\033[035m  [\033[037m10\033[035m] \033[037m{{==\033[031mSU\033[032mBCS\033[034mRI\033[033mBE\033[037m  Youtube Saya==}}")
+print ("\033[035m  [\033[037m99\033[035m] \033[032m{=Info Tools=} \033[037m") 
+Fck = str(input("Masukan Pilihan Anda: "))
+
+if Fck == "1" or Fck == "01":
+  print ("\033[033mMenginstall Metasploit.......")
+  time.sleep(0.3)
+  os.system("pkg update && pkg upgrade")
+  os.system("pkg install unstable-repo")
+  os.system("pkg install metasploit")
+  time.sleep(1)
+  print ("\033[032mInstallasi Sudah Selesai!!!!!!")
+  time.sleep(2)
+  print ("Gunakan Perintah msfconsole Untuk masuk ke metasploit")
+
+elif Fck == "99":
+  time.sleep(0.5)
+  print ("\033[032mSedang Membuka Document:v")
+  time.sleep(2.5)
+  print ("""\033[037m			TERIMAKASIH BANYAK!!!!!
+
+    DAN YAH TERIMAKASIH JIGA BUAT KALIAN YANG SUDAH
+    MENGSUBSCRIBE.
+
+    CAHNNEL SAYA TeaM Cyber SAYA SANGAT SENANG:V😅
+    HMM YA SEPERTI INI INFONYA:V SELAMAT BERJUMPA KEMBALI
+    MWHEHEHEHE""")
+
+elif Fck == "10":
+  os.system("am start https://m.youtube.com/channel/UCVaKVhhcdngVG1aGjzDLFmg")
